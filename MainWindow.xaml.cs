0e99518b58f7a4ccb20d2c8b362ba8c03a6fc48b
gatheesha <gatheesha.official@gmail.com>
2026-06-17 08:09:17 +0530

    Revert "added the earningsWindow and its code behind and fixes some errors"
    This reverts commit 18dff7977c5e30858b920731907b68833e744248.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,14 +11,6 @@ namespace wada
             MainContentFrame.Content = new ProjectsView();
             BtnProjects.Click += (s, e) => MainContentFrame.Content = new ProjectsView();
             BtnClients.Click += (s, e) => MainContentFrame.Content = new ClientsView();
-            BtnEarnings.Click += (s, e) =>
-            {
-                var win = new EarningsWindow
-                {
-                    Owner = this
-                };
-                win.Show();
-            };
         }
     }
 }
